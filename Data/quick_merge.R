12c4b3bc9fa0d2488834670a8df8ee585bc4ca06
Spaolizzi <sophiepaolizzi@gmail.com>
2022-04-18 12:42:15 -0400

    glmers for reversal

--- Data/quick_merge.R
+++ Data/quick_merge.R
@@ -12,7 +12,7 @@ load("~/github_repos/Cannon_Task/Data/qualtrics_qata/PUBS_Batch3_QR_Proc.Rdata")
 
 
 
-selected <- measures_df %>% dplyr::select(RandomID, gen, PAI_Tot, PAI_AI,
+selected <- measures_df %>% dplyr::select(RandomID, age, gen, PAI_Tot, PAI_AI,
                                           PAI_NR,
                                           PAI_SH,
                                           PID_neg_aff,
@@ -28,9 +28,13 @@ selected <- measures_df %>% dplyr::select(RandomID, gen, PAI_Tot, PAI_AI,
                                           PID_persev,PID_rest_aff,PID_perf,PID_rt,
                                           PID_sep_insec,PID_sub,PID_sus,PID_unusual,
                                           PID_neg_aff,PID_detach,PID_antag,PID_disinhib,
-                                          PID_psycho, STAI_Tot,STAI_T,STAI_S,PSWQ_Tot,BFAS_Tot,BFAS_V,BFAS_W, age) %>% rename(subject = RandomID) %>% mutate(subject = as.character(subject)) 
+                                          PID_psycho, STAI_Tot,STAI_T,STAI_S,PSWQ_Tot,
+                                          BFAS_Tot,BFAS_V,BFAS_W, age) %>% 
+  rename(subject = RandomID) %>% mutate(subject = as.character(subject)) 
+selected <- selected %>% mutate(sex = recode(gen, `1` = "female", `2` = "male", 
+                                             .default=NA_character_))
  
-df <- left_join(selected, within_effects, by = "subject")
+df <- left_join(selected, data, by = "subject")
                                                                                                                                                              
 
 save(df, file = paste0(data_dir,'base_effects_data.RData'))
